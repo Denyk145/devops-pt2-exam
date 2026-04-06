28b12f900ea5f083dc97adfdceedfba012a9d825
Stasiv Denys <denys@MacBook-Air-Stasiv.local>
2026-04-06 23:20:28 +0300

    task3 setup

--- task3/scripts/deploy-release.sh
+++ task3/scripts/deploy-release.sh
@@ -5,7 +5,6 @@ docker pull YOUR_DOCKERHUB_USERNAME/devops-exam-pt2:1.0.0
 docker rm -f app-release || true
 
 docker run -d \
-	--name app-release \
-	-p 8003:8000 \
-	YOUR_DOCKERHUB_USERNAME/devops-exam-pt2:1.0.0
-
+  --name app-release \
+  -p 8003:8000 \
+  YOUR_DOCKERHUB_USERNAME/devops-exam-pt2:1.0.0
