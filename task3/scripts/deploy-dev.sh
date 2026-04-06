ec3958360aea9b1dfb4cdf5a5f056e0019f348ef
Stasiv Denys <denys@MacBook-Air-Stasiv.local>
2026-04-06 23:18:47 +0300

    task3 setup

--- task3/scripts/deploy-dev.sh
+++ task3/scripts/deploy-dev.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -e
+
+docker pull YOUR_DOCKERHUB_USERNAME/devops-exam-pt2:develop
+docker rm -f app-dev || true
+
+docker run -d \
+	--name app-dev \
+	-p 8002:8000 \
+	YOUR_DOCKERHUB_USERNAME/devops-exam-pt2:develop
 
